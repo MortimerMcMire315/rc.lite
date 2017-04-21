8dfe682d16c2c0fc86327c8282d1f51af76cc2bf
MortimerMcMire315 <seth.a.yoder@gmail.com>
2017-04-21 16:32:15 +0000

    Add nginx.vim

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ cp .vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c VundleInstall
+mkdir -p ~/.vim/syntax
+cp nginx.vim ~/.vim/syntax
+echo "au BufRead,BufNewFile /etc/nginx/*,/usr/local/nginx/conf/* if &ft == '' | setfiletype nginx | endif" >> ~/.vim/filetype.vim
