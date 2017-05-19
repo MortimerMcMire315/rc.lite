c90f1f7ecf4ee5681927a7dc6845d2b43958195e
MortimerMcMire315 <seth.a.yoder@gmail.com>
2017-05-19 16:38:26 +0000

    Back up old configs; use conditionals to avoid errors in install script

--- install.sh
+++ install.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
+echo "Backing up .bashrc..."
+mv ~/.bashrc ~/.bashrc.bak
+echo "Installing new .bashrc..."
 cp .bashrc ~/.bashrc
+
+echo "Backing up .tmux.conf..."
+mv ~/.tmux.conf ~/.tmux.conf.bak
+echo "Installing new .tmux.conf..."
 cp .tmux.conf ~/.tmux.conf
+
+echo "Backing up .vimrc..."
+mv ~/.vimrc ~/.vimrc.bak
+echo "Installing new .vimrc..."
 cp .vimrc ~/.vimrc
-mkdir -p ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+[[ -d ~/.vim/bundle/Vundle.vim ]] ||
+(
+    echo "Downloading Vundle..."
+    mkdir -p ~/.vim/bundle
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+)
+echo "Installing Vim plugins..."
 vim -c 'VundleInstall' -c 'qa!'
+
+echo "Installing nginx Vim syntax file..."
 mkdir -p ~/.vim/syntax
 cp nginx.vim ~/.vim/syntax
-echo "au BufRead,BufNewFile /etc/nginx/*,/usr/local/nginx/conf/* if &ft == '' | setfiletype nginx | endif" >> ~/.vim/filetype.vim
+
+[[ ! -f ~/.vim/filetype.vim ]] || [[ -z "$(grep nginx ~/.vim/filetype.vim)" ]] &&
+(
+    echo "Adding nginx detection to filetype.vim..."
+    echo "au BufRead,BufNewFile /etc/nginx/*,/usr/local/nginx/conf/* if &ft == '' | setfiletype nginx | endif" >> ~/.vim/filetype.vim
+)
 source ~/.bashrc
